f8a9eaa517242b9844b5fb96df07ce9bab4dfc69
morteza omidipoor <omidiporm@gmail.com>
2020-03-06 02:20:21 +0330

    define conceptual relationship in db

--- djkarmap/main_app/models.py
+++ djkarmap/main_app/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+
+class JobGroup(models.Model):
+    pass
+
+
+class Job(models.Model):
+    pass
+
+
+class Skil(models.Model):
+    pass
+
+
+class Employee(models.Model):
+    pass
+
+
+class Employer(models.Model):
+    pass
+
+
+class EmployeeOrder(models.Model):
+    pass
+
+
+class EmployerOrder(models.Model):
+    pass
+
