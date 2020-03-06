d8c2a70328f64e905dbc51b8c07e03ffe66d0dc9
morteza omidipoor <omidiporm@gmail.com>
2020-03-07 02:00:32 +0330

    display all orders job using add ListView in home page

--- djkarmap/djkarmap/settings.py
+++ djkarmap/djkarmap/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'main_app',
     'pages',
+    
 ]
 
 MIDDLEWARE = [
--- djkarmap/pages/views.py
+++ djkarmap/pages/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from main_app.models import JobOrder
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = JobOrder
     template_name = 'pages/home.html'
+    context_object_name = 'all_orders'
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
