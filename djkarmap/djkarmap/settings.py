1c041bc5816bd8ea992c0435fb21d54fd3a1707f
morteza omidipoor <omidiporm@gmail.com>
2020-03-06 23:59:11 +0330

    add django contrib app and geo model admin

--- djkarmap/djkarmap/settings.py
+++ djkarmap/djkarmap/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # add my app
+    'django.contrib.gis',
     'main_app',
 ]
 
--- djkarmap/main_app/admin.py
+++ djkarmap/main_app/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
-# Register your models here.
+from .models import JobGroup, Job, Skil, Employee, Employer, JobOrder
+
+admin.site.register(JobGroup)
+admin.site.register(Job, admin.GeoModelAdmin)
+admin.site.register(Skil)
+admin.site.register(Employee)
+admin.site.register(Employer)
+admin.site.register(JobOrder)
+
