5df64bbbe280d55f9409047ac02433a5d60b171b
morteza omidipoor <omidiporm@gmail.com>
2020-03-06 19:18:00 +0330

    add sample user data

--- djkarmap/main_app/sample_data/sample_data.py
+++ djkarmap/main_app/sample_data/sample_data.py
@@ -3,11 +3,11 @@ from .models import JobGroup, Job, Skil, Employee, Employer, JobOrder
 
 
 num_user = 5
-User.objects.exclude(username='admin').delete()
 
 for i in range(num_user):
     user = User.objects.create_user('username %s'%i, 'username%s@gmail.com'%i, 'User123456789')
     user.first_name = 'morteza %s'%i
     user.last_name = 'morteza %s'%i
     user.save()
+
 
