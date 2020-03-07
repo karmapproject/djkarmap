d991cb2c0e8ad9cb2e93a39b1623603790fa0e70
morteza omidipoor <omidiporm@gmail.com>
2020-03-08 01:20:48 +0330

    create detailview for job

--- djkarmap/pages/urls.py
+++ djkarmap/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AboutPageView, HomePageView
+from .views import JobPageListView, JobPageDetailView, AboutPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', JobPageListView.as_view(), name='jobs'),
+    path('job/<int:pk>/', JobPageDetailView.as_view(), name='job_detail'),
     path('about/', AboutPageView.as_view(), name='about'),
 ]
--- djkarmap/pages/views.py
+++ djkarmap/pages/views.py
@@ -1,13 +1,20 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from main_app.models import Job
 
 
-class HomePageView(ListView):
+class JobPageListView(ListView):
     model = Job
-    template_name = 'pages/home.html'
+    template_name = 'pages/jobs.html'
     context_object_name = 'all_Jobs'
 
+
+class JobPageDetailView(DetailView):
+
+    template_name = 'pages/job_detail.html'
+    context_object_name = 'job'
+    queryset = Job.objects.order_by('-created')
+    
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
