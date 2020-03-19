1db811a36da5f5e64635136c5a31ca17c11cfbfb
morteza omidipoor <omidiporm@gmail.com>
2020-03-20 03:19:47 +0330

    filter search form based on job group and user_type

--- djkarmap/api/urls.py
+++ djkarmap/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . views import JobApiView, JobDetailView
 
 urlpatterns = [
-    path('job/',  JobApiView.as_view()),
+    path('job_search/<int:group>/<int:user_type>/',  JobApiView.as_view()),
     path('job/<int:pk>/', JobDetailView.as_view()),
 ]
--- djkarmap/api/views.py
+++ djkarmap/api/views.py
@@ -5,11 +5,25 @@ from main_app.models import Job
 from .serializers import JobSerializer
 
 
-
+# TODO use post or get is better
 class JobApiView(generics.ListAPIView):
 
-    queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    def get_queryset(self):
+        # save parameters sended by search form
+        group = self.kwargs['group']
+        user_type = self.kwargs['user_type']
+        # when user select employee he want find jobs requested by employer
+        # so in form we use 1 for 'KJ and 2 for 'KF'
+        # and so so we changed it for get require queryset
+
+        if user_type == 1:
+            user_type = 'KF'
+        else:
+            user_type = 'KJ'        
+        
+        return Job.objects.filter(user__user_type=user_type, group=group)[0:100]
 
 
 class JobDetailView(generics.RetrieveAPIView):
